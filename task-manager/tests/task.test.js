5d355778ee4dc0477b54544cc5dc26e6258c1751
funasty1985 <tony.f.liang@gmail.com>
2020-03-01 22:14:49 +0800

    149. Testing with Task Data

--- task-manager/tests/task.test.js
+++ task-manager/tests/task.test.js
@@ -1,7 +1,12 @@
 const request = require('supertest')
 const Task = require('../src/models/task')
 const app = require('../src/app')
-const { userOne, userOneId, setupDatabase } = require('./fixtures/db')
+const { 
+    userOne, 
+    userTwo, 
+    taskOne, 
+    setupDatabase 
+    } = require('./fixtures/db')
 
 
 beforeEach(setupDatabase);
@@ -19,3 +24,22 @@ test('Should create task for user', async () => {
     expect(task).not.toBeNull()
     expect(task.completed).toEqual(false)
 })
+
+test('Should get tasks for user one', async () =>{
+    const response = await request(app)
+        .get('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .expect(200)
+
+    expect(response.body).toHaveLength(2)
+})
+
+test('User two should not be able to delete tasks of user one', async () =>{
+    const response = await request(app)
+        .delete(`/tasks/${taskOne._id}`)  
+        .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)  
+        .expect(400)
+
+    const task = await Task.findById(taskOne._id)
+    expect(task).not.toBeNull()
+ })
